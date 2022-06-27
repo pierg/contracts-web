a7cc3023638087ec0f5cc00fad7b9bf20d255c02
guillaume <gasser.guillaume@gmail.com>
2022-06-27 16:30:07 +0200

    working on Contracts

---
+++ frontend/src/views/custom/CromeContracts.js
@@ -0,0 +1,72 @@
+import React from "react";
+import CustomHeader from "../../components/Crome/CustomHeader";
+import cromecontractsheaderscards from "../../_texts/custom/cromecontractsheaderscards";
+import ComponentsView from "../../components/Custom/ComponentsView";
+import CustomNavButton from "../../components/Custom/CustomNavButton";
+
+export default class CromeContracts extends React.Component {
+    state = {
+        headerStates: [true, false],
+        currentTabOpen: 0
+    }
+
+    toggleNew = (e, actionToggle, disabled) => {
+        if(disabled) return
+        let newHeaderStates = Array(this.state.headerStates.length).fill(false);
+        newHeaderStates[actionToggle] = true
+        this.setState({
+                currentTabOpen: actionToggle,
+                headerStates: newHeaderStates
+            }
+        )
+
+    }
+
+
+    render() {
+
+    let page;
+    if(this.state.headerStates[0]){
+        page = <ComponentsView/>
+}
+    else{
+        page = <></>
+}
+        return (
+            <>
+                <CustomHeader
+                    {...cromecontractsheaderscards}
+                    color={"purple"}
+                    states={this.state.headerStates}
+                    clickable={false}
+                />
+                <div className="flex justify-evenly relative top--12">
+                    <div>
+                        <CustomNavButton
+                            open={this.state.currentTabOpen}
+                            itemsLength="2"
+                            type={"back"}
+                            toggleNew={this.toggleNew}
+                            href="#/contracts"
+                        />
+
+                    </div>
+                    <div>
+                        <CustomNavButton
+                            open={this.state.currentTabOpen}
+                            itemsLength="2"
+                            type={"continue"}
+                            toggleNew={this.toggleNew}
+                            href="#/contracts"
+                        />
+                    </div>
+                </div>
+
+                <div className="mx-40 my-6">
+                    {page}
+
+                </div>
+            </>
+        )
+    }
+}
