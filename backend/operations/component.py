bb53ac5aea2a58c69d531f7994679664cae64f2a
Ronzon Mathis <mathis.ronzon@ecole.ensicaen.Fr>
2022-06-29 13:20:48 +0200

    save of compenent works

--- backend/operations/component.py
+++ backend/operations/component.py
@@ -1,18 +1,11 @@
 import os
 import json
 from os import walk
+from pathlib import Path
 
 from backend.shared.paths import component_path
 from crome_component.component import Component
-
-HEADER_SYMBOL = "**"
-NAME_HEADER = "**NAME**"
-DESCRIPTION_HEADER = "**DESCRIPTION**"
-INPUTS_HEADER = "**INPUTS**"
-OUTPUTS_HEADER = "**OUTPUTS**"
-ASSUMPTION_HEADER = "**ASSUMPTION**"
-GUARANTEES_HEADER = "**GUARANTEES**"
-COMMENT_CHAR = "#"
+from crome_component.component.component_spec import NAME_HEADER, _check_header
 
 
 class ComponentOperation:
@@ -38,7 +31,48 @@ class ComponentOperation:
         component_folder = component_path(session_id)
         if not os.path.exists(component_folder):
             os.makedirs(component_folder)
-        # TODO : create the txt file and put all the information inside
+
+        _, _, filenames = next(walk(component_folder))
+
+        greatest_id = -1 if len(filenames) == 0 else int(max(filenames)[0:4])
+        greatest_id += 1
+
+        file_checked = ComponentOperation.__check_if_component_exist(data["name"], component_folder)
+        if file_checked:
+            file = component_folder / file_checked
+        else:
+            file = component_folder / f"{str(greatest_id).zfill(4)}.txt"
+
+        with open(file, "w") as file:
+            file.write("**NAME**\n\n")
+            file.write(f"\t{data['name']}\n")
+
+            file.write("\n**DESCRIPTION**\n\n")
+            file.write(f"\t{data['description']}\n")
+
+            file.write("\n**INPUTS**\n\n")
+            for elt in data["inputs"]:
+                file.write(f"\t{elt['name']} ({elt['type']})\n")
+
+            file.write("\n**OUTPUTS**\n\n")
+            for elt in data["outputs"]:
+                file.write(f"\t{elt['name']} ({elt['type']})\n")
+
+            file.write("\n**ASSUMPTIONS**\n\n")
+            file.write("\tPL:\n")
+            for elt in data["assumptions"]["PL"]:
+                file.write(f"\t\t{elt}\n")
+            file.write("\tLTL:\n")
+            for elt in data["assumptions"]["LTL"]:
+                file.write(f"\t\t{elt}\n")
+
+            file.write("\n**GUARANTEES**\n\n")
+            file.write("\tPL:\n")
+            for elt in data["guarantees"]["PL"]:
+                file.write(f"\t\t{elt}\n")
+            file.write("\tLTL:\n")
+            for elt in data["guarantees"]["LTL"]:
+                file.write(f"\t\t{elt}\n")
 
     @staticmethod
     def delete_component(name, session_id) -> bool:
@@ -54,7 +88,34 @@ class ComponentOperation:
         return False
 
     @staticmethod
-    def _check_header(line: str) -> tuple[str, bool]:
-        if line.startswith(HEADER_SYMBOL):
-            return line.strip(), True
-        return line.strip(), False
+    def __get_name_from_file(file_path) -> str:
+        if not os.path.exists(file_path):
+            return ""
+
+        with open(file_path, 'r') as ifile:
+            line_header = ""
+            name = ""
+            for line in ifile:
+                line, header = _check_header(line)
+                if not line:
+                    continue
+
+                if header:
+                    if line_header == NAME_HEADER:
+                        return name[:-1]
+                    if line == NAME_HEADER:
+                        line_header = line
+                else:
+                    if line_header == NAME_HEADER:
+                        name += line.strip() + " "
+        return ""
+
+    @staticmethod
+    def __check_if_component_exist(name: str, component_folder: Path) -> str:
+        if not os.path.exists(component_folder):
+            return ""
+        _, _, filenames = next(walk(component_folder))
+        for filename in filenames:
+            name_found = ComponentOperation.__get_name_from_file(component_folder / filename)
+            if name_found == name:
+                return filename
