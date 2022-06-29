7fdab84b599c3352fbfae14f31d0e93ea82e282b
Ronzon Mathis <mathis.ronzon@ecole.ensicaen.Fr>
2022-06-29 12:37:37 +0200

    start to create class Component

---
+++ crome_component/component/__init__.py
@@ -0,0 +1,47 @@
+from dataclasses import dataclass
+from pathlib import Path
+from crome_component.component.component_spec import ComponentSpec, _check_header, NAME_HEADER, DESCRIPTION_HEADER
+
+
+@dataclass
+class Component:
+    name: str = ""
+    description: str = ""
+    spec: ComponentSpec = None
+
+
+    @classmethod
+    def from_file(cls, file_path: Path):
+        spec = ComponentSpec.from_file(file_path)
+        name = ""
+        description = ""
+        with open(file_path, 'r') as ifile:
+
+            line_header = ""
+
+            for line in ifile:
+                line, header = _check_header(line)
+
+                if not line:
+                    continue
+
+                elif header:
+                    if line == NAME_HEADER:
+                        if line_header == "":
+                            line_header = line
+                        else:
+                            Exception("File format not supported")
+                    elif line == DESCRIPTION_HEADER:
+                        if line_header == NAME_HEADER:
+                            line_header = line
+                        else:
+                            Exception("File format not supported")
+
+                else:
+
+                    if line_header == NAME_HEADER:
+                        name += line.strip() + " "
+                    elif line_header == DESCRIPTION_HEADER:
+                        description += line.strip() + " "
+
+        return cls(name=name[:-1], description=description[:-1], spec=spec)
