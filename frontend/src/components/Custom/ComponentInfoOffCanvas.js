cae020337b027ccbc6b8ebd45d1728fa81c39e30
Théa <thealinot@gmail.com>
2022-07-01 10:35:58 +0200

    better looking info offcanvas

--- frontend/src/components/Custom/ComponentInfoOffCanvas.js
+++ frontend/src/components/Custom/ComponentInfoOffCanvas.js
@@ -9,40 +9,39 @@ function ComponentInfoOffCanvas(props) {
     let guarantees = []
     if (props.component) {
         for (let i=0; i<props.component.inputs.length; i++) {
-            inputs.push(<div key={"inputs"+i}>{props.component.inputs[i].name + " (" + props.component.inputs[i].type+")"}</div>)
+            inputs.push(<div className="pb-2" key={"inputs"+i}>{props.component.inputs[i].name + " (" + props.component.inputs[i].type+")"}</div>)
         }
         for (let i=0; i<props.component.outputs.length; i++) {
-            outputs.push(<div key={"outputs"+i}>{props.component.outputs[i].name + " (" + props.component.outputs[i].type+")"}</div>)
+            outputs.push(<div className="pb-2" key={"outputs"+i}>{props.component.outputs[i].name + " (" + props.component.outputs[i].type+")"}</div>)
         }
 
-        assumptions = <div>{"LTL : "+props.component.assumptions.LTL}<br></br>{"PL : "+props.component.assumptions.PL }</div>
-        guarantees = <div>{"LTL : "+props.component.guarantees.LTL}<br></br>{"PL : "+props.component.guarantees.PL }</div>
+        assumptions = <div style={{lineHeight:"1.5rem"}}>{"LTL : "+props.component.assumptions.LTL}<br></br>{"PL : "+props.component.assumptions.PL }</div>
+        guarantees = <div style={{lineHeight:"1.5rem"}}>{"LTL : "+props.component.guarantees.LTL}<br></br>{"PL : "+props.component.guarantees.PL }</div>
     }
 
-    return (<Offcanvas show={props.show} placement="end" onHide={props.handleClose}>
+    return (<Offcanvas show={props.show} placement="end" className="pl-3" onHide={props.handleClose}>
         <Offcanvas.Header closeButton>
           <Offcanvas.Title>{props.component && (<div className="title title-up text-2xl">{props.component.name}</div>)}</Offcanvas.Title>
         </Offcanvas.Header>
         <Offcanvas.Body>
             {props.component && (
                 <div className="m-auto">
-
-                    <div className="py-3 text-lg text-justify">{props.component.description}</div>
+                    <div className="py-3 text-md text-justify">{props.component.description}</div>
                     <div className="py-3 text-md">
                         <h4 className="font-bold title-up mb-2 inline">{componentInfo.info.texts.info.inputs} : </h4>
-                        {inputs}
+                        <div className="pl-2 pt-2">{inputs}</div>
                     </div>
                     <div className="py-3 text-md">
                         <h4 className="font-bold title-up mb-2 inline">{componentInfo.info.texts.info.outputs} : </h4>
-                        {outputs}
+                        <div className="pl-2 pt-2">{outputs}</div>
                     </div>
                     <div className="py-3 text-md">
                         <h4 className="font-bold title-up mb-2 inline">{componentInfo.info.texts.info.assumptions} : </h4>
-                        {assumptions}
+                        <div className="pl-2 pt-2">{assumptions}</div>
                     </div>
                     <div className="py-3 text-md">
                         <h4 className="font-bold title-up mb-2 inline">{componentInfo.info.texts.info.guarantees} : </h4>
-                        {guarantees}
+                        <div className="pl-2 pt-2">{guarantees}</div>
                     </div>
             </div>)}
 
