0068a97a3016d311fbf51af1b22de36a2fb67fe4
Ronzon Mathis <mathis.ronzon@ecole.ensicaen.Fr>
2022-07-18 18:08:53 +0200

    refractor signal handler

--- backend/app.py
+++ backend/app.py
@@ -8,7 +8,6 @@ from typing import Any
 from flask import Flask, Response, request
 from flask_socketio import SocketIO, emit
 
-from backend.operations.component import ComponentOperation
 from backend.operations.library import LibraryOperation
 from backend.operations.connection import ConnectionOperation
 from backend.shared.paths import (
@@ -175,156 +174,7 @@ def display_message(data) -> None:
     )
 
 
-@socketio.on("get-components")
-def get_components() -> None:
-    """
-        get the synthesis created by the user and the examples.
-    """
-    session_id = str(request.args.get("id"))
-    list_examples = ComponentOperation.get_components(session_id)
-
-    emit("receive-components", list_examples, room=request.sid)
-
-
-# The signal for the component
-@socketio.on("save-component")
-def save_component(data) -> None:
-    """
-        get the synthesis created by the user and the examples.
-    """
-    session_id = str(request.args.get("id"))
-    now = time.localtime(time.time())
-    component = data["new_component"]
-    print(f"Old Name : {data['old_name']}")
-    name: str = component["name"]
-
-    # Detection error
-    error = 0
-    if len(component["inputs"]) == 0 and len(component["outputs"]) == 0:
-        error = 1
-        emit(
-            "send-message",
-            strftime("%H:%M:%S", now) + ' The component "' + name + '" has not been saved. There are no inputs and '
-                                                                    'outputs',
-            room=request.sid,
-        )
-
-    if error == 1:
-        emit(
-            "send-notification",
-            {"crometypes": "error", "content": "The component can't be added, see console for more information"},
-            room=request.sid
-        )
-    else:
-        emit("component-saved", True, room=request.sid)
-
-        try:
-            ComponentOperation.save_component(data=component, old_name=data["old_name"], session_id=session_id)
-            send_message_to_user(content='The component "' + name + '" has been saved.',
-                                 room_id=request.sid, crometype="success")
-        except KeyError as keyError:
-            emit(
-                "send-message",
-                strftime("%H:%M:%S", now) + ' The component "' + name + '" has not been saved. Error with the entry '
-                                                                        f'of the LTL/Pattern \n KeyError : {keyError}',
-                room=request.sid,
-            )
-
-
-@socketio.on("delete-component")
-def delete_component(name) -> None:
-    """
-    We delete the json file related to the component id given by the frontend.
-    """
-    session_id = str(request.args.get("id"))
-
-    is_deleted = ComponentOperation.delete_component(name, session_id)
-    if is_deleted:
-        send_message_to_user(f"The component '{name}' has been deleted.", request.sid, "success")
-    else:
-        send_message_to_user(f"The component '{name}' has not been deleted", request.sid, "error")
-    emit("component-deleted", is_deleted, room=request.sid)
-
-
-@socketio.on("download-components")
-def download_components(data):
-    """
-        Download the txt file of component(s)
-    """
-    list_component = []
-    session_id = request.args.get("id")
-    for name in data["names"]:
-        raw = ComponentOperation.get_raw_component(name, session_id)
-        if raw:
-            data = {"name": name, "file": raw}
-            list_component.append(data)
-    emit("components-downloaded", list_component, room=request.sid)
-
-
-@socketio.on("upload-component")
-def upload_component(component_file):
-    """
-        Upload a component
-    """
-    session_id = request.args.get("id")
-    is_saved = ComponentOperation.save_component_file(component_file, session_id)
-    emit("upload-done", True, room=request.sid)
-    if not is_saved:
-        send_message_to_user("The file does not have the right structure", request.sid, "error")
-    else:
-        send_message_to_user("The component has been uploaded", request.sid, "success")
-
-
-# The signal for the library
-@socketio.on("get-library")
-def get_library() -> None:
-    """
-        Get all the library that exists in the session folder
-    """
-    session_id = str(request.args.get("id"))
-    list_library = LibraryOperation.get_library(session_id)
-    emit("receive-library", list_library, room=request.sid)
-
-
-@socketio.on("add-components-to-library")
-def add_components_to_library(data) -> None:
-    """
-        Add components to the library wanted
-    """
-    session_id = str(request.args.get("id"))
-    LibraryOperation.add_to_library(data["name"], data["components"], session_id)
-
-    emit("add-to-library-done", True, room=request.sid)
-    send_message_to_user(f"The components have been added to the library {data['name']}", request.sid, "success")
-
-
-@socketio.on("remove-component-from-library")
-def remove_component_from_library(data) -> None:
-    """
-        Remove a component from a library
-    """
-    session_id = str(request.args.get("id"))
-    is_deleted = LibraryOperation.remove_from_library(data["name"], data["component"], session_id)
-
-    emit("remove-from-library-done", is_deleted, room=request.sid)
-
-
-@socketio.on("remove-library")
-def remove_library(library_name) -> None:
-    """
-        Remove a library
-    """
-    session_id = str(request.args.get("id"))
-    is_deleted = LibraryOperation.remove_library(library_name, session_id)
-    emit("remove-library-done", is_deleted, room=request.sid)
-    if is_deleted:
-        send_message_to_user(f"The library {library_name} has been deleted.", request.sid, "success")
-    else:
-        send_message_to_user(f"The library {library_name} has not been deleted.", request.sid, "error")
-
-
 # Connections Handler
-
 @socketio.on("save-connection")
 def save_connection(data):
     """
@@ -334,6 +184,10 @@ def save_connection(data):
     ConnectionOperation.save_connection(data, session_id)
     emit("save-connection-done", True, room=request.sid)
 
+
+import backend.signal_handler.component
+import backend.signal_handler.connection
+import backend.signal_handler.library
 
 if __name__ == "__main__":
     socketio.run(app, host="0.0.0.0")
---
+++ backend/signal_handler/component.py
@@ -0,0 +1,113 @@
+import time
+from time import strftime
+
+from flask import request
+from flask_socketio import emit
+
+from backend.app import send_message_to_user
+from backend.operations.component import ComponentOperation
+
+try:
+    from __main__ import socketio
+except ImportError:
+    from backend.app import socketio
+
+
+@socketio.on("get-components")
+def get_components() -> None:
+    """
+        get the synthesis created by the user and the examples.
+    """
+    session_id = str(request.args.get("id"))
+    list_examples = ComponentOperation.get_components(session_id)
+
+    emit("receive-components", list_examples, room=request.sid)
+
+
+# The signal for the component
+@socketio.on("save-component")
+def save_component(data) -> None:
+    """
+        get the synthesis created by the user and the examples.
+    """
+    session_id = str(request.args.get("id"))
+    now = time.localtime(time.time())
+    component = data["new_component"]
+    print(f"Old Name : {data['old_name']}")
+    name: str = component["name"]
+
+    # Detection error
+    error = 0
+    if len(component["inputs"]) == 0 and len(component["outputs"]) == 0:
+        error = 1
+        emit(
+            "send-message",
+            strftime("%H:%M:%S", now) + ' The component "' + name + '" has not been saved. There are no inputs and '
+                                                                    'outputs',
+            room=request.sid,
+        )
+
+    if error == 1:
+        emit(
+            "send-notification",
+            {"crometypes": "error", "content": "The component can't be added, see console for more information"},
+            room=request.sid
+        )
+    else:
+        emit("component-saved", True, room=request.sid)
+
+        try:
+            ComponentOperation.save_component(data=component, old_name=data["old_name"], session_id=session_id)
+            send_message_to_user(content='The component "' + name + '" has been saved.',
+                                 room_id=request.sid, crometype="success")
+        except KeyError as keyError:
+            emit(
+                "send-message",
+                strftime("%H:%M:%S", now) + ' The component "' + name + '" has not been saved. Error with the entry '
+                                                                        f'of the LTL/Pattern \n KeyError : {keyError}',
+                room=request.sid,
+            )
+
+
+@socketio.on("delete-component")
+def delete_component(name) -> None:
+    """
+    We delete the json file related to the component id given by the frontend.
+    """
+    session_id = str(request.args.get("id"))
+
+    is_deleted = ComponentOperation.delete_component(name, session_id)
+    if is_deleted:
+        send_message_to_user(f"The component '{name}' has been deleted.", request.sid, "success")
+    else:
+        send_message_to_user(f"The component '{name}' has not been deleted", request.sid, "error")
+    emit("component-deleted", is_deleted, room=request.sid)
+
+
+@socketio.on("download-components")
+def download_components(data):
+    """
+        Download the txt file of component(s)
+    """
+    list_component = []
+    session_id = request.args.get("id")
+    for name in data["names"]:
+        raw = ComponentOperation.get_raw_component(name, session_id)
+        if raw:
+            data = {"name": name, "file": raw}
+            list_component.append(data)
+    emit("components-downloaded", list_component, room=request.sid)
+
+
+@socketio.on("upload-component")
+def upload_component(component_file):
+    """
+        Upload a component
+    """
+    session_id = request.args.get("id")
+    is_saved = ComponentOperation.save_component_file(component_file, session_id)
+    emit("upload-done", True, room=request.sid)
+    if not is_saved:
+        send_message_to_user("The file does not have the right structure", request.sid, "error")
+    else:
+        send_message_to_user("The component has been uploaded", request.sid, "success")
