49941f6f1dc0e0e9f2af0f1ae97e61f8894dd31f
Ronzon Mathis <mathis.ronzon@gmail.com>
2022-08-04 17:12:16 +0200

    change file for the server

---
+++ entrypoint.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+echo "...updating repositories from server..."
+
+cd /home/crome-cgg
+git reset --hard
+git config pull.rebase true
+git pull
+
+cd /home/crome-contracts
+git reset --hard
+git config pull.rebase true
+git pull
+
+cd /home/crome-logic
+git reset --hard
+git config pull.rebase true
+git pull
+
+cd /home/crome-synthesis
+git reset --hard
+git config pull.rebase true
+git pull
+
+cd /home/contracts-web
+git reset --hard
+git config pull.rebase true
+git pull
+
+echo "repositories updated"
+
+if [ $# -eq 0 ]
+  then
+    source run.sh
+else
+    source run.sh "$@"
+fi
