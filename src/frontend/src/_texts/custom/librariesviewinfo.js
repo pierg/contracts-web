b989ffa3dd953c908d6875d9998578400e8fd1a4
Gasser-G <gasser.guillaume@gmail.com>
2022-08-04 20:34:22 +0200

    added tooltip

--- src/frontend/src/_texts/custom/librariesviewinfo.js
+++ src/frontend/src/_texts/custom/librariesviewinfo.js
@@ -1,22 +1,29 @@
 const textProps = {
-  info: {
-    title: "LIBRARIES",
-    icon: "fa-solid fa-folder fa-2xl",
-    addLibrary: {
-      size: "sm",
-      color: "gray",
-      icon: "fa-solid fa-plus fa-2xl text-lightBlue-700",
+    info: {
+        title: "LIBRARIES",
+        icon: "fa-solid fa-folder fa-2xl",
+        addLibrary: {
+            size: "sm",
+            color: "gray",
+            icon: "fa-solid fa-plus fa-2xl text-lightBlue-700",
+        },
+        deleteLibrary: {
+            size: "sm",
+            color: "red",
+            icon: "fas fa-trash-alt",
+        },
     },
-    deleteLibrary: {
-      size: "sm",
-      color: "red",
-      icon: "fas fa-trash-alt",
+    demoLibrary: {
+        message: [
+            {
+                content: "This is a demo library, you can’t add or remove components from it.",
+            },
+            {
+                content: "Select it and click on “connect” to go to the next page"
+            }],
+        icon: "ml-1 text-lightBlue-700 text-2xl fas fa-info-circle"
+
     },
-  },
-  demoLibraryHoover : {
-    text :  "This is a demo library, you can’t add or remove components from it\n" +
-            "Select it and click on “connect” to go to the next page\n"
-  }
 };
 
 export default textProps;
--- src/frontend/src/components/Custom/LibraryView.js
+++ src/frontend/src/components/Custom/LibraryView.js
@@ -1,101 +1,126 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import librariesviewsinfo from "../../_texts/custom/librariesviewinfo";
-import { Tooltip } from "react-tippy";
+import {Tooltip} from "react-tippy";
 import Button from "../Elements/Button";
-import { Modal } from "reactstrap";
+import {Modal} from "reactstrap";
 import ModalLibrary from "./ModalLibrary";
+import contractsconnect from "../../_texts/custom/contractsconnect";
 
 function LibraryView(props) {
-  const [triggerAddLibrary, setTriggerAddLibrary] = useState(false);
+    const [triggerAddLibrary, setTriggerAddLibrary] = useState(false);
 
-  function selectLibrary(id) {
-    if (props.libraries[id] !== props.selectedLibrary) {
-      props.setSelectedLibrary(id);
-    } else {
-      props.setSelectedLibrary(null);
+    function selectLibrary(id) {
+        if (props.libraries[id] !== props.selectedLibrary) {
+            props.setSelectedLibrary(id);
+        } else {
+            props.setSelectedLibrary(null);
+        }
     }
-  }
 
-  function deleteLibrary(i) {
-    props.deleteLibrary(props.libraries[i].name);
-  }
+    function deleteLibrary(i) {
+        props.deleteLibrary(props.libraries[i].name);
+    }
 
-  const tmp_libraries = [];
-  let libraryClass = "";
-  for (let i = 0; i < props.libraries.length; i++) {
-    libraryClass = "border-b-1 text-blueGray-700 text-lg p-3 cursor-pointer";
-    if (i === 0) libraryClass += " border-t-1";
-    if (props.libraries[i] === props.selectedLibrary) {
-      libraryClass += " bg-blueGray-200 hover:bg-blueGray-300";
-    } else libraryClass += "  hover:bg-blueGray-100";
+    const tmp_libraries = [];
+    let libraryClass = "";
+    for (let i = 0; i < props.libraries.length; i++) {
+        libraryClass = "border-b-1 text-blueGray-700 text-lg p-3 cursor-pointer";
+        if (i === 0) libraryClass += " border-t-1";
+        if (props.libraries[i] === props.selectedLibrary) {
+            libraryClass += " bg-blueGray-200 hover:bg-blueGray-300";
+        } else libraryClass += "  hover:bg-blueGray-100";
 
-    tmp_libraries.push(
-      <li key={i} className={libraryClass} onClick={() => selectLibrary(i)}>
-        {props.libraries[i].name}
-        {!props.libraries[i].default && (
-          <>
-            <div className="float-right">
-              <Tooltip html="delete" position="top" arrow="true">
-                <Button
-                  size={librariesviewsinfo.info.deleteLibrary.size}
-                  color={librariesviewsinfo.info.deleteLibrary.color}
-                  onClick={(e) => {
-                    e.stopPropagation();
-                    deleteLibrary(i);
-                  }}
-                >
-                  <i className={librariesviewsinfo.info.deleteLibrary.icon} />
-                </Button>
-              </Tooltip>
-            </div>
-          </>
-        )}
-      </li>
-    );
-  }
+        tmp_libraries.push(
+            <li key={i} className={libraryClass} onClick={() => selectLibrary(i)}>
 
-  return (
-    <div className="px-3 pb-3 w-4/12 relative flex flex-col flex-initial min-w-library break-words bg-white rounded shadow-md h-fit mb-5">
-      <div className="flex flex-wrap justify-between p-4 text-center">
-        <div>
-          <i className={librariesviewsinfo.info.icon + " align-middle pr-4"}></i>
-          <span className="fs-4 font-bold text-blueGray-500 align-middle">
+                {props.libraries[i].name}
+                {!props.libraries[i].default && (
+                    <>
+                        <div className="float-right">
+                            <Tooltip html="delete" position="top" arrow="true">
+                                <Button
+                                    size={librariesviewsinfo.info.deleteLibrary.size}
+                                    color={librariesviewsinfo.info.deleteLibrary.color}
+                                    onClick={(e) => {
+                                        e.stopPropagation();
+                                        deleteLibrary(i);
+                                    }}
+                                >
+                                    <i className={librariesviewsinfo.info.deleteLibrary.icon}/>
+                                </Button>
+                            </Tooltip>
+                        </div>
+                    </>
+                )}
+
+                {props.libraries[i].default && (
+                    <>
+                        <div className="float-right ">
+                            <Tooltip
+                                html={
+                                    <div className="flex flex-col text-center ">
+                                        {librariesviewsinfo.demoLibrary.message.map((prop, key) => (
+                                            <div className="mb-1" key={key}>
+                                                <div>{prop.content}</div>
+                                            </div>
+                                        ))}
+                                    </div>       }
+                                position="right"
+                                arrow="true">
+
+                                <i className={librariesviewsinfo.demoLibrary.icon}/>
+                            </Tooltip>
+                        </div>
+                    </>
+                )}
+
+            </li>
+        );
+    }
+
+    return (
+        <div
+            className="px-3 pb-3 w-4/12 relative flex flex-col flex-initial min-w-library break-words bg-white rounded shadow-md h-fit mb-5">
+            <div className="flex flex-wrap justify-between p-4 text-center">
+                <div>
+                    <i className={librariesviewsinfo.info.icon + " align-middle pr-4"}></i>
+                    <span className="fs-4 font-bold text-blueGray-500 align-middle">
             {librariesviewsinfo.info.title}
           </span>
-        </div>
+                </div>
 
-        <div className="">
-          <Tooltip title="Add a library" position="right" arrow="true">
-            <Button
-              size={librariesviewsinfo.info.addLibrary.size}
-              color={librariesviewsinfo.info.addLibrary.color}
-              onClick={() => setTriggerAddLibrary(true)}
-            >
-              <i className={librariesviewsinfo.info.addLibrary.icon}></i>
-            </Button>
-          </Tooltip>
-          <Modal
-            isOpen={triggerAddLibrary}
-            autoFocus={false}
-            toggle={() => setTriggerAddLibrary(false)}
-            className={"modal-dialog-centered"}
-          >
-            <ModalLibrary
-              // Can be a future feature
-              // components={props.components}
-              addLibrary={(libraryName, componentsName) =>
-                props.addLibrary(libraryName, componentsName)
-              }
-              close={() => setTriggerAddLibrary(false)}
-            />
-          </Modal>
+                <div className="">
+                    <Tooltip title="Add a library" position="right" arrow="true">
+                        <Button
+                            size={librariesviewsinfo.info.addLibrary.size}
+                            color={librariesviewsinfo.info.addLibrary.color}
+                            onClick={() => setTriggerAddLibrary(true)}
+                        >
+                            <i className={librariesviewsinfo.info.addLibrary.icon}></i>
+                        </Button>
+                    </Tooltip>
+                    <Modal
+                        isOpen={triggerAddLibrary}
+                        autoFocus={false}
+                        toggle={() => setTriggerAddLibrary(false)}
+                        className={"modal-dialog-centered"}
+                    >
+                        <ModalLibrary
+                            // Can be a future feature
+                            // components={props.components}
+                            addLibrary={(libraryName, componentsName) =>
+                                props.addLibrary(libraryName, componentsName)
+                            }
+                            close={() => setTriggerAddLibrary(false)}
+                        />
+                    </Modal>
+                </div>
+            </div>
+            <div className="max-h-400-px mt-3 mx-4 mb-4">
+                <ul>{tmp_libraries}</ul>
+            </div>
         </div>
-      </div>
-      <div className="overflow-auto max-h-400-px mt-3 mx-4 mb-4">
-        <ul>{tmp_libraries}</ul>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default LibraryView;
