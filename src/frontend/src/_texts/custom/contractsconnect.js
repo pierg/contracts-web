47f378ac988264c4889861e852f2e13890d10034
Gasser-G <gasser.guillaume@gmail.com>
2022-08-04 21:02:33 +0200

    add tooltips

--- src/frontend/src/_texts/custom/contractsconnect.js
+++ src/frontend/src/_texts/custom/contractsconnect.js
@@ -1,53 +1,61 @@
 const textProps = {
-  instances: {
-    instancesTitle: "Instances",
-    instanceStyle: "text-2xl font-bold text-blueGray-500 uppercase m-auto",
-    messages: [
-      {
-        title: "Step 1 :",
-        content: "Select variables you want to connect by unfolding instances.",
-      },
-      {
-        title: "Step 2 :",
-        content: "Click on the arrow button to create the connection.",
-      },
-      {
-        title: "Step 3 :",
-        content: "Repeat",
-      },
-    ],
-        demoMessages: "Here are the instances, you can unfold them to see their output and input\n" +
-                      "Connections on the right are already created\n" +
-                      "Click on “Create system” to go to generated your system\n",
-  },
-  connections: {
-    connectionsTitle: "Connections",
-    connectionsStyle: "text-2xl font-bold text-blueGray-500 uppercase mx-4 mr-4",
-  },
-  addButton: {
-    size: "sm",
-    color: "gray",
-    icon: "fa-solid fa-plus fa-2xl text-lightBlue-700",
-  },
-  clearButton: {
-    size: "sm",
-    color: "gray",
-    icon: "fas fa-trash-alt fa-2xl text-lightBlue-700",
-  },
-  addConnectionButton: {
-    size: "sm",
-    color: "gray",
-    icon: "fa-solid fa-arrow-right fa-2xl text-lightBlue-700",
-  },
-  downloadButton: {
-    size: "sm",
-    color: "gray",
-    icon: "fa-solid fa-download fa-2xl text-lightBlue-700",
-  },
-  deleteButton: {
-    size: "sm",
-    color: "red",
-    icon: "fas fa-trash-alt",
-  },
+    instances: {
+        instancesTitle: "Instances",
+        instanceStyle: "text-2xl font-bold text-blueGray-500 uppercase m-auto",
+        messages: [
+            {
+                title: "Step 1 :",
+                content: "Select variables you want to connect by unfolding instances.",
+            },
+            {
+                title: "Step 2 :",
+                content: "Click on the arrow button to create the connection.",
+            },
+            {
+                title: "Step 3 :",
+                content: "Repeat",
+            },
+        ],
+        demoMessage: [
+            {
+                content: "Here are the instances, you can unfold them to see their output and input"
+            },
+            {
+                content: "Connections on the right are already created"
+            },
+            {
+                content: "Click on “Create system” to go to generated your system"
+            }
+        ]
+    },
+    connections: {
+        connectionsTitle: "Connections",
+        connectionsStyle: "text-2xl font-bold text-blueGray-500 uppercase mx-4 mr-4",
+    },
+    addButton: {
+        size: "sm",
+        color: "gray",
+        icon: "fa-solid fa-plus fa-2xl text-lightBlue-700",
+    },
+    clearButton: {
+        size: "sm",
+        color: "gray",
+        icon: "fas fa-trash-alt fa-2xl text-lightBlue-700",
+    },
+    addConnectionButton: {
+        size: "sm",
+        color: "gray",
+        icon: "fa-solid fa-arrow-right fa-2xl text-lightBlue-700",
+    },
+    downloadButton: {
+        size: "sm",
+        color: "gray",
+        icon: "fa-solid fa-download fa-2xl text-lightBlue-700",
+    },
+    deleteButton: {
+        size: "sm",
+        color: "red",
+        icon: "fas fa-trash-alt",
+    },
 };
 export default textProps;
